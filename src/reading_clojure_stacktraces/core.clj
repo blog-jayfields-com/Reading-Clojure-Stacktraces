3474034b45637c9f529131fba0ca6edf4387bb34
Jay Fields <git@jayfields.com>
2012-06-24 10:23:15 -0400

    add skeleton

---
+++ src/reading_clojure_stacktraces/core.clj
@@ -0,0 +1,1 @@
+(ns reading-clojure-stacktraces.core)
---
+++ test/reading_clojure_stacktraces/test/core.clj
@@ -0,0 +1,6 @@
+(ns reading-clojure-stacktraces.test.core
+  (:use [reading-clojure-stacktraces.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
